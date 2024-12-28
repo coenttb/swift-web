341af8c68e8a0ceea488913c6e54fbe0c0b71ae3
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-28 22:36:13 +0100

    add Field.contentType convenience

--- Sources/UrlFormCoding/Util.swift
+++ Sources/UrlFormCoding/Util.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UrlFormEncoding
+import URLRouting
 
 extension UrlFormDecoder {
   @MainActor public static let `default`: UrlFormDecoder = {
@@ -9,16 +10,18 @@ extension UrlFormDecoder {
     }()
 }
 
-// public let formDecoder: UrlFormDecoder = {
-//    let decoder = UrlFormDecoder()
-//    decoder.parsingStrategy = .bracketsWithIndices
-//    return decoder
-// }()
-
 extension DateFormatter {
   @MainActor public static let form: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
+}
+
+extension URLRouting.Field {
+    public static func contentType(_ type: () -> Value) -> Self {
+        Field("Content-Type") {
+            type()
+        }
+    }
 }
