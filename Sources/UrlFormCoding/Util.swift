ae73a0a6cef75805a91750a29787693959cbe521
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2025-01-04 12:29:28 +0100

    wip

--- Sources/UrlFormCoding/Util.swift
+++ Sources/UrlFormCoding/Util.swift
@@ -3,7 +3,7 @@ import UrlFormEncoding
 import URLRouting
 
 extension UrlFormDecoder {
-  @MainActor public static let `default`: UrlFormDecoder = {
+    @MainActor public static let `default`: UrlFormDecoder = {
         let decoder = UrlFormDecoder()
         decoder.parsingStrategy = .bracketsWithIndices
         return decoder
@@ -11,7 +11,7 @@ extension UrlFormDecoder {
 }
 
 extension DateFormatter {
-  @MainActor public static let form: DateFormatter = {
+    @MainActor public static let form: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
@@ -22,6 +22,69 @@ extension URLRouting.Field {
     public static func contentType(_ type: () -> Value) -> Self {
         Field("Content-Type") {
             type()
+        }
+    }
+}
+
+extension URLRouting.Field<String> {
+    // Convenience properties for common Content-Type values
+    public static var applicationJSON: Self {
+        Field.contentType { "application/json" }
+    }
+    
+    public static var json: Self {
+        .applicationJSON
+    }
+    
+    public static var applicationFormURLEncoded: Self {
+        Field.contentType { "application/x-www-form-urlencoded" }
+    }
+    
+    public static var formURLEncoded: Self {
+        .applicationFormURLEncoded
+    }
+    
+    public static var multipartFormData: Self {
+        Field.contentType { "multipart/form-data" }
+    }
+    
+    public static var textPlain: Self {
+        Field.contentType { "text/plain" }
+    }
+    
+    public static var textHTML: Self {
+        Field.contentType { "text/html" }
+    }
+    
+    public static var html: Self {
+        .textHTML
+    }
+    
+    public static var applicationXML: Self {
+        Field.contentType { "application/xml" }
+    }
+    
+    public static var xml: Self {
+        .applicationXML
+    }
+    
+    public static var applicationOctetStream: Self {
+        Field.contentType { "application/octet-stream" }
+    }
+    
+    public static var octetStream: Self {
+        .applicationOctetStream
+    }
+}
+
+extension URLRouting.Field<String> {
+    public enum form {
+        public static var multipart: URLRouting.Field<String> {
+            Field.contentType { "multipart/form-data" }
+        }
+
+        public static var urlEncoded: URLRouting.Field<String> {
+            Field.contentType { "application/x-www-form-urlencoded" }
         }
     }
 }
