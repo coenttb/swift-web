d1e02124fd0f1324001ba0667ba4422f1a9bb15c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-28 15:52:13 +0100

    wip

--- Sources/SwiftWeb/JWT.Token.swift
+++ Sources/SwiftWeb/JWT.Token.swift
@@ -12,31 +12,26 @@ public enum JWT {}
 extension JWT {
     public struct Token: Codable, Hashable, Sendable, ExpressibleByStringLiteral, ExpressibleByStringInterpolation {
         public let value: String
-        public let type: String?
-        public let expiresIn: TimeInterval?
         
         public init(
-            value: String,
-            type: String? = "Bearer",
-            expiresIn: TimeInterval?
+            value: String
         ) {
             self.value = value
-            self.type = type
-            self.expiresIn = expiresIn
         }
         
         public enum CodingKeys: String, CodingKey {
             case value = "token"
-            case type
-            case expiresIn = "exp"
         }
         
         public init(stringLiteral value: String) {
-            self = .init(value: value, expiresIn: nil)
+            self = .init(value: value)
         }
     }
 }
 
+extension JWT.Token {
+    public var token: String { value }
+}
 extension JWT.Token: TestDependencyKey {
-    public static let testValue: Self = .init(value: "test", expiresIn: 100)
+    public static let testValue: Self = .init(value: "test")
 }
